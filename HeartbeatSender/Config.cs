cf4029f0ec8a8ea759bc544a3da885dbb04fc7bb
Misaka_ZeroTwo <45165615+Misaka-ZeroTwo@users.noreply.github.com>
2023-05-18 23:50:02 +0800

    initial commut

---
+++ HeartbeatSender/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Interfaces;
+
+namespace HeartbeatSender
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        public string URL { get; set; } = "";
+
+        public int Period { get; set; } = 15;
+    }
+}
---
+++ HeartbeatSender/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using Exiled.API.Features;
+
+namespace HeartbeatSender
+{
+    public class Plugin : Plugin<Config>
+    {
+        private Thread heartbeatThread;
+        public override void OnEnabled()
+        {
+            heartbeatThread = new Thread(new ThreadStart(HeartbeatThread));
+            heartbeatThread.Start();
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            heartbeatThread.Abort();
+            base.OnDisabled();
+        }
+
+        private void HeartbeatThread()
+        {
+            if (string.IsNullOrEmpty(Config.URL))
+            {
+                heartbeatThread.Abort();
+            }
+
+            HttpClient client = new HttpClient();
+            for (; ; )
+            {
+                try
+                {
+                    client.SendAsync(new HttpRequestMessage(HttpMethod.Post, Config.URL));
+                    Log.Debug("Heartbeat message sent");
+                } catch (Exception e)
+                {
+                    Log.Error("HeartbeatSender throws an exception: " + e);
+                }
+
+                Thread.Sleep(Config.Period * 1000);
+            }
+        }
+    }
+}
